e4737f4c91d820b20e6805938076b24eda9a2140
Nyks06 <vincent.d.vielle+github@gmail.com>
2015-11-19 00:54:56 +0100

    InitSignalsListeningFor first version

--- abstraction.go
+++ abstraction.go
@@ -18,6 +18,12 @@ const (
 
 type dbusAbstraction struct {
 	conn *dbus.Conn
+	sign map[string]chan *dbus.Signal
+}
+
+//GetConn method return the current instance of *dbus.Conn
+func (d *dbusAbstraction) GetConn() *dbus.Conn {
+	return d.conn
 }
 
 //InitSession method is the first callable. It permits to init a session (Session or System) over the bus and request a name on it.
@@ -28,6 +34,10 @@ func (d *dbusAbstraction) InitSession(s SessionType, n string) error {
 	var err error
 	var conn *dbus.Conn
 
+	if d.conn != nil {
+		return errors.New("[DBUS ABSTRACTION ERROR - initSession - Session already initialized]")
+	}
+
 	if s == SESSION {
 		conn, err = dbus.SessionBus()
 	} else {
@@ -36,10 +46,9 @@ func (d *dbusAbstraction) InitSession(s SessionType, n string) error {
 	if err != nil {
 		return err
 	}
-	d.conn = conn
 
 	if n != "" {
-		reply, err := d.conn.RequestName(n, dbus.NameFlagDoNotQueue)
+		reply, err := conn.RequestName(n, dbus.NameFlagDoNotQueue)
 		if err != nil {
 			return err
 		}
@@ -47,14 +56,28 @@ func (d *dbusAbstraction) InitSession(s SessionType, n string) error {
 			return errors.New("[DBUS ABSTRACTION ERROR - initSession - name already taken]")
 		}
 	}
+
+	d.conn = conn
+	d.sign = make(map[string]chan *dbus.Signal)
+
 	return nil
 }
 
-//ExportMethods method is usable each time the user want to export an interface over the bus
+//ExportMethods method is usable each time the user wants to export an interface over the bus
 //Parameters :
 //              m -> interface{}     : the interface containing the methods the user wants to export
 //              p -> dbus.ObjectPath : the objectPath in which the user wants to export methods
 //              i -> string          : the interface in which the user wants to export methods
 func (d *dbusAbstraction) ExportMethods(m interface{}, p dbus.ObjectPath, i string) {
 	d.conn.Export(m, p, i)
+}
+
+//ListenSignals method is usable to set a new 'listener'. This listener will fill a channel each time a signal is send
+//Parameters :
+//              p -> string           : the ObjectPath of the sender
+//              n -> string           : the name of the sender
+//              i -> string           : the interface of the sender
+
+func (d *dbusAbstraction) InitSignalsFromSender(p string, n string, i string) {
+	d.conn.BusObject().Call("org.freedesktop.DBus.AddMatch", 0, "type='signal',path='"+p+"',interface='"+i+"', sender='"+n+"'")
 }
