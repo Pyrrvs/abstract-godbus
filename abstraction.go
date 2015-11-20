a3f6fa653d08d0cf0f6ef23403ee2ce449282c69
Nyks06 <vincent.d.vielle+github@gmail.com>
2015-11-20 01:39:00 +0100

    Modification and add the skeleton of signals handling

--- abstraction.go
+++ abstraction.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"strings"
 
 	"github.com/nyks06/dbus"
 )
@@ -16,9 +18,17 @@ const (
 	SYSTEM SessionType = iota
 )
 
+//The dbusSignal type is a copy of dbus.Signal type, used to parse received signals
+type dbusAbsSignal struct {
+	recv    *dbus.Signal
+	signame string
+}
+
 type dbusAbstraction struct {
-	conn *dbus.Conn
-	sign map[string]chan *dbus.Signal
+	conn       *dbus.Conn
+	recv       chan *dbus.Signal
+	sigmap     map[string]chan *dbusAbsSignal
+	sigsenders []string
 }
 
 //GetConn method return the current instance of *dbus.Conn
@@ -58,8 +68,9 @@ func (d *dbusAbstraction) InitSession(s SessionType, n string) error {
 	}
 
 	d.conn = conn
-	d.sign = make(map[string]chan *dbus.Signal)
-
+	d.sigmap = make(map[string]chan *dbusAbsSignal)
+	d.recv = make(chan *dbus.Signal, 1024)
+	go d.signalsHandler()
 	return nil
 }
 
@@ -77,6 +88,44 @@ func (d *dbusAbstraction) ExportMethods(m interface{}, p dbus.ObjectPath, i stri
 //              p -> string           : the ObjectPath of the sender
 //              n -> string           : the name of the sender
 //              i -> string           : the interface of the sender
-func (d *dbusAbstraction) InitSignalsListeningFor(p string, n string, i string) {
-	d.conn.BusObject().Call("org.freedesktop.DBus.AddMatch", 0, "type='signal',path='"+p+"',interface='"+i+"', sender='"+n+"'")
+//              s -> string           : the signal sent
+func (d *dbusAbstraction) ListenSignalFromSender(p string, n string, i string, s string) {
+	listened := false
+	for _, elem := range d.sigsenders {
+		if elem == n {
+			listened = true
+		}
+	}
+	if listened {
+
+		//check if the entry already exist in the map [entry is sender.member]
+		//yes : do nothing
+		//no  : create the entry and the channel
+	} else {
+		_ = append(d.sigsenders, n)
+		d.conn.BusObject().Call("org.freedesktop.DBus.AddMatch", 0, "type='signal',path='"+p+"',interface='"+i+"', sender='"+n+"'")
+		//add the specific channel and entry to the map
+	}
+}
+
+func (d *dbusAbstraction) getSignalName(s string) string {
+	tmp := strings.Split(s, ".")
+	return tmp[len(tmp)-1]
+}
+
+//signalsHandler method is called in the InitSession method. It permits to handle our signals and put them in the map
+//This method run in a special goroutines. It read each signal comming from a registered sender and put it in the sigmap
+func (d *dbusAbstraction) signalsHandler() {
+	d.conn.Signal(d.recv)
+	for v := range d.recv {
+
+		if _, ok := d.sigmap[v.Name]; ok {
+			// d.sigmap[t.signame] = make(chan *dbusSignalAbstraction, 1024)
+			var t dbusAbsSignal
+			t.recv = v
+			t.signame = v.Name
+			d.sigmap[v.Name] <- &t
+			fmt.Println(v)
+		}
+	}
 }
